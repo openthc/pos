c2f7f0487ac1370f4c78dc7ba680afd0c5e828a0
openthc <code@openthc.com>
2020-02-23 09:46:21 +0000

    user internal SQL Record

--- lib/B2C/Sale.php
+++ lib/B2C/Sale.php
@@ -5,9 +5,7 @@
 
 namespace App\B2C;
 
-use Edoceo\Radix\DB\SQL;
-
-class Sale extends \OpenTHC\SQL\Record
+class Sale extends \App\SQL\Record
 {
 	protected $_table = 'b2c_sale';
 
@@ -21,7 +19,7 @@ class Sale extends \OpenTHC\SQL\Record
 
 		$sql = 'SELECT * FROM b2c_sale WHERE id = ?';
 		$arg = array($gri);
-		$res = SQL::fetch_row($sql, $arg);
+		$res = $this->_dbc->fetchRow($sql, $arg);
 		if (empty($res)) {
 			$S = new Sale();
 			$S['id'] = $id;
@@ -48,9 +46,9 @@ class Sale extends \OpenTHC\SQL\Record
 
 	function getItems()
 	{
-		$sql = 'SELECT * FROM b2c_sale_item WHERE b2c_sale_id = ?';
+		$sql = 'SELECT * FROM b2c_sale_item WHERE b2c_sale_id = ? ORDER BY sort, id';
 		$arg = array($this->_data['id']);
-		$res = SQL::fetch_all($sql, $arg);
+		$res = $this->_dbc->fetchAll($sql, $arg);
 		return $res;
 	}
 
--- lib/B2C/Sale/Item.php
+++ lib/B2C/Sale/Item.php
@@ -5,9 +5,11 @@
 
 namespace App\B2C\Sale;
 
-use Edoceo\Radix\DB\SQL;
-
-class Item extends \OpenTHC\SQL\Record
+class Item extends \App\SQL\Record
 {
 	protected $_table = 'b2c_sale_item';
+
+	const FLAG_TAX_EXCISE = 0x00010000;
+	const FLAG_TAX_RETAIL = 0x00020000;
+
 }
