38a408f0de0edc82e3741da99e1264feba8139d4
openthc <code@openthc.com>
2020-03-19 01:39:46 +0000

    print relay pdf-blob to custom print-server

--- lib/Module/Settings.php
+++ lib/Module/Settings.php
@@ -16,13 +16,29 @@ class Settings extends \OpenTHC\Module\Base
 			return $this->view->render($RES, 'page/settings.html', $data);
 		});
 
+		$a->get('/printer', function($REQ, $RES, $ARG) {
+			$data = [
+				'Page' => ['title' => 'Settings :: Printer' ],
+			];
+			return $this->view->render($RES, 'page/settings/printer.html', $data);
+		});
+
+		$a->get('/receipt', function($REQ, $RES, $ARG) {
+			$data = [
+				'Page' => ['title' => 'Settings :: Receipt' ],
+			];
+			return $this->view->render($RES, 'page/settings/receipt.html', $data);
+		});
+
+		$a->get('/receipt/preview', 'App\Controller\Settings\Receipt\Preview');
+
 		// $this->get('', function($REQ, $RES, $ARG) {
 		// 	$data = array();
 		// 	$this->view->render($RES, 'page/settings/index.html', $data);
 		// });
-	
+
 		// // Reports
 		// $this->group('/report', 'App\Module\Report');
-				
+
 	}
 }
---
+++ webroot/js/pos-printer.js
@@ -0,0 +1,54 @@
+/**
+ * Print Helper
+ */
+
+POS.Printer = {
+
+	printGoogleCloud: function() {},
+
+	printLocalBrowser: function() {},
+
+	/**
+	 * Download the Document as PDF and then send to our HTTP Print Queue
+	 */
+	printLocalNetwork: function(pdf_url, printer_url)
+	{
+		console.log('POS.Printer.printLocalNetwork()');
+
+		// Big AJAX
+		$.ajax({
+			type: 'POST',
+			url: pdf_url,
+			data: {
+				a: 'send-print',
+			},
+			dataType: 'binary',
+			xhrFields: {
+				responseType: 'blob',
+			},
+			success: function(body, ret) {
+
+				// Then another Big AJAX
+				var FD = new FormData();
+				FD.append('a', 'print-file');
+				FD.append('file', body);
+
+				$.ajax({
+					type: "POST",
+					url: printer_url,
+					cache: false,
+					contentType: false,
+					processData: false,
+					data: FD,
+					xhrFields:{
+						responseType: 'blob'
+					},
+				});
+			}
+		});
+
+	},
+
+	printServerConnect: function() {},
+
+};
