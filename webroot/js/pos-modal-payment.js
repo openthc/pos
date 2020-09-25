3f3aec897133ce5fde9c893d47c6655d37c221bd
openthc <code@openthc.com>
2020-09-25 23:16:42 +0000

    Patch the JS on Modals

--- webroot/js/pos-modal-payment.js
+++ webroot/js/pos-modal-payment.js
@@ -1,5 +1,176 @@
+/**
+ *
+ */
+
+var ppCashPaid_List = new Array();
+
+
+function ppFormUpdate()
+{
+	var full = Weed.POS.sale.due;
+	// var cash = parseFloat($('#pp-cash-pay').val(), 10);
+	var cash = parseFloat($('#pp-cash-pay').text(), 10);
+	var need = (full - cash);
+	var back = (cash - full);
+
+	console.log('ppFormUpdate(' + cash + ', ' + full + ')');
+
+
+	if (cash < full) {
+
+		$('#amount-paid-wrap').removeClass('alert-success');
+		$('#amount-paid-wrap').addClass('alert-secondary');
+
+		$('#amount-need-wrap').removeClass('alert-danger alert-secondary alert-success');
+		$('#amount-need-wrap').addClass('alert-warning');
+		$('#amount-need-hint').text('Due:');
+		$('#amount-need-value').text(need.toFixed(2));
+
+	} else if (cash === full) {
+
+		// $('#pos-back-due').html('0.00');
+		$('#amount-paid-wrap').removeClass('alert-secondary alert-success');
+		$('#amount-paid-wrap').addClass('alert-success');
+
+		$('#amount-need-wrap').removeClass('alert-danger alert-secondary alert-success alert-warning');
+		$('#amount-need-wrap').addClass('alert-success');
+		$('#amount-need-hint').text('Perfect!');
+		$('#amount-need-value').text('0.00');
+
+		// $('.pos-cost-due').addClass('text-success').removeClass('text-warning');
+		// $('#pp-cash-pay').addClass('text-success').removeClass('text-danger').removeClass('text-warning');
+
+		// $('#pos-payment-over').show().addClass('alert alert-success');
+		$('#pos-payment-commit').removeAttr('disabled');
+
+	} else if (cash > full) {
+
+		$('#amount-paid-wrap').removeClass('alert-secondary alert-success');
+		$('#amount-paid-wrap').addClass('alert-success');
+
+		// Making Change
+		$('#amount-need-wrap').removeClass('alert-danger alert-secondary alert-success');
+		$('#amount-need-wrap').addClass('alert-danger');
+		$('#amount-need-hint').text('Change:');
+		$('#amount-need-value').text( back.toFixed(2) );
+		// $('#pos-back-due').html( back.toFixed(2) )
+
+		// $('.pos-cost-due').addClass('text-warning').removeClass('text-success');
+		// $('#pp-cash-pay').addClass('text-warning').removeClass('text-danger').removeClass('text-success');
+		// $('#pp-card-pay').removeClass('text-danger').removeClass('text-success').removeClass('text-warning');
+
+		//$('#pos-back-due').addClass('text-warning').removeClass('text-danger').removeClass('text-success');
+		// $('#pos-payment-over').show().addClass('alert alert-danger');
+		$('#pos-payment-commit').removeAttr('disabled');
+
+	}
+
+	if (ppCashPaid_List.length == 0) {
+		$('#pos-pay-undo').prop('disabled', true);
+	} else {
+		$('#pos-pay-undo').prop('disabled', false);
+	}
+
+}
+
+
+function ppAddCash(n)
+{
+	console.log('ppAddCash');
+
+	var full = $('#pos-cost-due').data('amount');
+
+	var add = parseFloat( $(n).data('amount'), 10 );
+	var cur = parseFloat( $('#pp-cash-pay').text(), 10 );
+	if (!cur) cur = 0;
+
+	var cash = (cur + add);
+	$('#pp-cash-pay').text( cash.toFixed(2)  );
+
+	var card = full - cash;
+	$('#pp-card-pay').val( card.toFixed(2)  );
+
+	ppCashPaid_List.push(add);
+
+	ppFormUpdate();
+
+}
+
+function ppAddCard()
+{
+	console.log('ppAddCard');
+
+	var need = $('#payment-need').val();
+	$('#pp-card-confirm').val(need);
+
+	//Weed.modal('shut');
+
+	// var arg = $('#psi-form').serializeArray();
+	// $('#modal-content-wrap').load('/pos/pay', arg, function() {
+	// $.post('/pos/pay', arg, function(res) {
+
+	// var x = $('#card-modal').clone();
+	// $(x).find('#pp-card-confirm').attr('id', 'pp-card-prompt');
+	//Weed.modal( $('#card-modal') );
+	//$('#card-modal').show();
+
+
+	// Weed.modal('#card-modal');
+	// $('#pp-card-confirm').val( $('#pp-card-pay').val() );
+	// Weed.modal('#card-modal');
+	// });
+}
 
 $(function() {
+
+	$('.pp-cash').on('click touchend', function(e) {
+		ppAddCash(this);
+		e.preventDefault();
+		e.stopPropagation();
+		return false;
+	});
+
+	$('.pp-card').on('click touchend', function(e) {
+		ppAddCard();
+		e.preventDefault();
+		e.stopPropagation();
+		return false;
+	});
+
+	// Focus on Select!
+	// $('#pp-cash-pay').on('focus', function(e) {
+	// 	console.log('focus');
+	// 	$(this).select();
+	// });
+	// $('#pp-cash-pay').on('mouseup', function(e) {
+	// 	console.log('mouseup');
+	// 	e.preventDefault();
+	// 	return false;
+	// });
+
+	$('#pp-cash-pay').on('keyup', function() {
+		ppFormUpdate();
+	});
+
+	// Reset my Form
+	$('#pos-pay-undo').on('click', function() {
+		$('#pp-cash-pay').text('0.00');
+		$('#pp-card-pay').text('0.00');
+		ppFormUpdate();
+	});
+
+	$(document.body).on('click', '#pos-card-back', function() {
+		//Weed.modal('shut');
+	});
+
+	$(document.body).on('click', '#pos-card-done', function() {
+		//Weed.modal('shut');
+	});
+
+	// $('#pp-cash-pay').focus().select();
+	// var ppcp = document.getElementById('pp-cash-pay');
+	// ppcp.focus();
+	// ppcp.select();
 
 	/**
 		Actual Payment Button
@@ -19,7 +190,9 @@ $(function() {
 		return false;
 
 	});
+
 });
+
 
 // Alternate Payment Method Modal
 /*
