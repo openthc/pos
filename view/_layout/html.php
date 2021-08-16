fcef95b07812d4323737c8991c642ba325e85457
openthc <code@openthc.com>
2021-08-16 19:19:15 +0000

    move from twig to natural php

--- view/_layout/html.php
+++ view/_layout/html.php
@@ -1,16 +1,40 @@
-{% extends "layout/base.html" %}
+<?php
+/**
+ * OpenTHC HTML Layout
+ */
 
-{% block html_head_link %}
-{{ parent() }}
-<link rel="stylesheet" href="/css/pos.css">
-{% endblock %}
+use Edoceo\Radix;
+use Edoceo\Radix\Layout;
+use Edoceo\Radix\Session;
 
-{% block html_body %}
-<body>
+header('content-type: text/html; charset=utf-8', true);
 
+$body_class_list = [];
+$m1_mode = preg_match('/^(open|mini|shut)$/', $_COOKIE['m1'], $m) ? $m[1] : 'open';
+$body_class_list[] = sprintf('m1-%s', $m1_mode);
 
-{% include "block/menu-zero.html" %}
-
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1, user-scalable=yes">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="theme-color" content="#069420">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-IIED/eyOkM6ihtOiQsX2zizxFBphgnv1zbe1bKA+njdFzkr6cDNy16jfIKWu4FNH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha256-rByPlHULObEjJ6XQxW/flG2r+22R5dKiAoef+aXWfik=" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" integrity="sha256-yMjaV542P+q1RnH6XByCPDfUFhmOafWbeLPmqKh11zo=" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.bootstrap4.min.css" integrity="sha256-F+DaKAClQut87heMIC6oThARMuWne8+WzxIDT7jXuPA=" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.openthc.com/css/www/0.0.2/main.css" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.openthc.com/css/www/0.0.2/menu-tlr.css" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/main.css">
+<title><?= h(strip_tags($_ENV['title'])) ?></title>
+<head>
+</head>
+<body class="<?= implode(' ', $body_class_list) ?>" data-menu-left-mode="<?= $m1_mode ?>">
+<div class="body-wrap">
+<?= $this->block('body-head.php') ?>
 
 {% if alert %}
 	<div class="container">
