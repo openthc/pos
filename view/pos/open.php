f93056ffb76304f30655c3a88e6fae2545598e0f
openthc <code@openthc.com>
2021-09-15 16:55:52 +0000

    make terminal open more fun

--- view/pos/open.php
+++ view/pos/open.php
@@ -19,49 +19,88 @@
 <input name="CSRF" type="hidden" value="<?= $data['CSRF'] ?>">
 
 <div class="container mt-4">
+	<div class="form-control form-control-lg" id="auth-code-view" style="font-size: 150%; font-weight: bold; letter-spacing: 1em; line-height:1; text-align: center;"></div>
+	<input id="auth-code" name="code" type="hidden">
 	<div class="numpad-wrap">
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">1</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">2</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">3</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">4</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">5</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">6</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">7</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">8</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">9</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-secondary btn-action" type="button"><i class="fas fa-arrow-left"></i></button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-primary btn-number" type="button">0</button></div>
-		<div class="numpad-grid"><button class="btn btn-outline-success btn-action" name="a" type="submit" value="auth-code"><i class="fas fa-arrow-right"></i></button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="1">1</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="2">2</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="3">3</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="4">4</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="5">5</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="6">6</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="7">7</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="8">8</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="9">9</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-secondary btn-action" type="button" value="x"><i class="fas fa-arrow-left"></i></button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-primary btn-action" type="button" value="0">0</button></div>
+		<div class="numpad-grid"><button class="btn btn-outline-success" name="a" type="submit" value="auth-code"><i class="fas fa-arrow-right"></i></button></div>
 	</div>
-
-	<input class="form-control" id="auth-code" name="code" type="hidden">
 
 </div>
 </form>
 
 
 <script>
+var char_list = [
+	'&alpha;'
+	, '&beta;'
+	, '&gamma;'
+	, '&delta;'
+	, '&epsilon;'
+	, '&zeta;'
+	, '&eta;'
+	, '&theta;'
+	, '&iota;'
+	, '&kappa;'
+	, '&lambda;'
+	, '&mu;'
+	, '&nu;'
+	, '&xi;'
+	, '&omicron;'
+	, '&pi;'
+	, '&rho;'
+	, '&sigma;'
+	, '&tau;'
+	, '&upsilon;'
+	, '&phi;'
+	, '&chi;'
+	, '&psi;'
+	, '&omega;'
+];
+
+// Everyones Favourite Shuffle
+// @see https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+var idx = char_list.length - 1;
+for (idx; idx > 0; idx--) {
+	const rnd = Math.floor(Math.random() * (idx + 1));
+	const tmp = char_list[idx];
+	char_list[idx] = char_list[rnd];
+	char_list[rnd] = tmp;
+}
+
 var code_list = [];
+var code_mask = [];
+var code_auth = document.querySelector('#auth-code');
+var code_view = document.querySelector('#auth-code-view');
 
 $(function() {
 
-	$('.btn-number').on('click', function() {
-		var action = this.getAttribute('data-action');
-		switch (action) {
-		case 'back':
+	$('.btn-action').on('click', function() {
+
+		var x = this.value;
+		if ('x' === this.value) {
 			code_list.pop();
-			break;
-		case 'next':
-			break;
+			code_mask.pop();
+		} else {
+			code_list.push(x);
+			var c = char_list[ code_list.length - 1 ];
+			code_mask.push(c);
 		}
+
+		code_auth.value = code_list.join('');
+		code_view.innerHTML = code_mask.join('');
+
 	});
 
-	$('.btn-number').on('click', function() {
-
-		var n = parseInt(this.textContent, 10);
-		code_list.push(n);
-
-		$('#auth-code').val( code_list.join('+') );
-	});
 });
 </script>
