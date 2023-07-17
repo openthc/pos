55f1d588c6cd4d15569d5d07591c7f582b935069
openthc <code@openthc.com>
2023-07-17 15:07:01 +0000

    update jwt open

--- lib/Controller/Auth/oAuth2/Open.php
+++ lib/Controller/Auth/oAuth2/Open.php
@@ -11,6 +11,9 @@ use OpenTHC\JWT;
 
 class Open extends \OpenTHC\Controller\Auth\oAuth2
 {
+	/**
+	 *
+	 */
 	function __invoke($REQ, $RES, $ARG)
 	{
 		// Clear Session
@@ -22,30 +25,44 @@ class Open extends \OpenTHC\Controller\Auth\oAuth2
 		$ret_path = $this->_get_return_path();
 
 		if ( ! empty($_GET['jwt'])) {
+
+			// $p = $this->getProvider();
+			// $sso = new \OpenTHC\Service('sso');
+			// $res = $sso->get('/api/jwt/verify?jwt=' . $_GET['jwt']);
+			// switch ($res['code']) {
+			// 	case 200:
+			// 		// OK
+			// 	default:
+			// 		return $RES->withJSON(['meta' => [ 'note' => 'Invalid Token [AOO-033]' ]], 400);
+			// }
+
+			$dbc = _dbc('auth');
+
 			try {
 
-				$chk = JWT::decode($_GET['jwt']);
+				$chk = JWT::decode_only($_GET['jwt']);
+				$key = $dbc->fetchOne('SELECT hash FROM auth_service WHERE id = :s0', [ ':s0' => $chk->body->iss ]);
+				$jwt = JWT::verify($_GET['jwt'], $key);
+
+				$_SESSION['Contact'] = [
+					'id' => $chk->sub,
+				];
+				if (empty($_SESSION['Contact']['id'])) {
+					return $RES->withJSON(['meta' => [ 'note' => 'Invalid Contact [AOO-035]' ]], 400);
+				}
 
 				$_SESSION['Company'] = [
-					'id' => $chk['company'],
+					'id' => $jwt->company,
 				];
-				$_SESSION['Contact'] = [
-					'id' => $chk['sub'],
-				];
-				$_SESSION['License'] = [
-					'id' => $chk['license'],
-				];
-
 				if (empty($_SESSION['Company']['id'])) {
-					return $RES->withJSON(['meta' => [ 'detail' => 'Invalid Company' ]], 400);
+					return $RES->withJSON(['meta' => [ 'note' => 'Invalid Company [AOO-042]' ]], 400);
 				}
 
-				if (empty($_SESSION['Contact']['id'])) {
-					return $RES->withJSON(['meta' => [ 'detail' => 'Invalid Contact' ]], 400);
-				}
-
+				$_SESSION['License'] = [
+					'id' => $chk->license,
+				];
 				if (empty($_SESSION['License']['id'])) {
-					return $RES->withJSON(['meta' => [ 'detail' => 'Invalid License' ]], 400);
+					return $RES->withJSON(['meta' => [ 'note' => 'Invalid License [AOO-049]' ]], 400);
 				}
 
 				return $RES->withRedirect('/auth/init');
