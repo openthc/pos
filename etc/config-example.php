4a5aca0dc657af4c2c2e10df09840c75ff063cec
openthc <code@openthc.com>
2023-09-22 20:47:25 +0000

    update sso / oauth config

--- etc/config-example.php
+++ etc/config-example.php
@@ -1,8 +1,16 @@
 <?php
 /**
- * OpenTHC POS Example Configuration
+ * OpenTHC POS Configuration Example
  */
 
+// 'Client ID, Could be Public Key'
+$my_client_id = '010PENTHCX0000SVC000000P0S'; // pos.openthc.example;
+$my_client_pk = 'pos.openthc.example';
+$my_client_sk = 'SK/pos.openthc.example';
+
+$cfg = [];
+
+// Database
 $cfg['database'] = [
 	'auth' => [
 		'hostname' => 'sql0',
@@ -18,31 +26,41 @@ $cfg['database'] = [
 	],
 ];
 
+// Redis
 $cfg['redis'] = [
 	'hostname' => '127.0.0.1',
 ];
 
+// Statsd
 $cfg['statsd'] = [
 	'hostname' => '127.0.0.1',
 ];
 
+// OpenTHC
 $cfg['openthc'] = [
 	'app' => [
-		'origin' => 'https://pos.openthc.example.com',
-		'public' => 'pos.openthc.example.com',
-		'secret' => 'SK/pos.openthc.example.com',
+		'origin' => 'https://app.openthc.example',
+		'public' => 'App Public Key',
+		'secret' => $my_client_sk,
 		// 'scope' => 'contact company',
 	],
 	'dir' => [
-		'origin' => 'https://dir.openthc.example.com'
+		'origin' => 'https://dir.openthc.example',
+		'public' => 'Directory Public Key',
+		// 'secret' => '',
 	],
 	'pipe' => [
-		'origin' => 'https://pipe.openthc.example.com',
+		'origin' => 'https://pipe.openthc.example',
 		// 'scope' => 'pipe cre',
 	],
+	'pos' => [
+		'id' => '/* POS Service ULID */',
+		'origin' => 'https://pos.openthc.example',
+	],
 	'sso' => [
-		'origin' => 'https://sso.openthc.example.com',
-		'secret' => 'SK/pos.openthc.example.com',
+		'origin' => 'https://sso.openthc.example',
+		'oauth-client-id' => '/* POS SERVICE ULID */',
+		'oauth-client-sk' => '/* POS SERVICE Client Secret */'
 		// 'scope' => 'contact company profile cre pos',
 	]
 ];
