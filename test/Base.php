484be235fa69469709c6cc0e9317eb47ce688109
openthc <code@openthc.com>
2024-04-08 21:45:27 +0000

    use parent assertValidResponse

--- test/Base.php
+++ test/Base.php
@@ -6,8 +6,8 @@
 
 namespace Test;
 
-class Base extends \PHPUnit\Framework\TestCase
-{
+class Base extends \OpenTHC\Test\Base {
+
 	protected $ghc; // API Guzzle HTTP Client
 	protected $raw; // Raw Response Buffer
 
@@ -36,42 +36,6 @@ class Base extends \PHPUnit\Framework\TestCase
 		$res = $this->ghc->post($url, array('json' => $arg));
 		return $res;
 	}
-
-	/**
-	 * Intends to become an assert wrapper for a bunch of common response checks
-	 * @param $res, Response Object
-	 * @return void
-	 */
-	function assertValidResponse($res, $code=200, $dump=null)
-	{
-			$this->raw = $res->getBody()->getContents();
-
-			$hrc = $res->getStatusCode();
-
-			if (empty($dump)) {
-					if ($code != $hrc) {
-							$dump = "HTTP $hrc != $code";
-					}
-			}
-
-			if (!empty($dump)) {
-					echo "\n<<< $dump <<< $hrc <<<\n{$this->raw}\n###\n";
-			}
-
-			$ret = \json_decode($this->raw, true);
-
-			$this->assertEquals($code, $res->getStatusCode());
-			$type = $res->getHeaderLine('content-type');
-			$type = strtok($type, ';');
-			$this->assertEquals('application/json', $type);
-			$this->assertIsArray($ret);
-
-			// $this->assertEmpty($ret['status']);
-			// $this->assertEmpty($ret['result']);
-
-			return $ret;
-	}
-
 
 	function _data_stash_get()
 	{
