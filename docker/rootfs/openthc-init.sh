78cfe384d7baef3d58cc5ce70bb745b92923e015
edoceo <code@edoceo.com>
2025-09-17 10:59:51 -0400

    improve the docker

--- docker/rootfs/openthc-init.sh
+++ docker/rootfs/openthc-init.sh
@@ -7,16 +7,29 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
-printenv | sort
+# printenv | sort
 
-if [ -f /first-run.php ]
+# PHP Debugger
+OPENTHC_DEBUG=${OPENTHC_DEBUG:-"false"}
+if [ "$OPENTHC_DEBUG" == "true" ]
 then
-	echo "RUN0"
-	php /first-run.php
-	rm /first-run.php
-else
-	echo "RUN1+"
+	echo "DEBUG ENABLED"
+	phpenmod xdebug
 fi
+
+# if [ -f /first-run.php ]
+# then
+# 	echo "RUN0"
+# 	php /first-run.php
+# 	rm /first-run.php
+# else
+# 	echo "RUN1+"
+# fi
+
+
+# The Service Init Script
+# php /opt/openthc/pos/init.php
+
 
 # Start Regular Way
 exec /usr/sbin/apache2 -DFOREGROUND
