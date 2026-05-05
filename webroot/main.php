c0760867b7cc7333983ff9c317a20bf67e02f83d
openthc <code@openthc.com>
2026-05-05 05:18:47 +0000

    clean up routes

--- webroot/main.php
+++ webroot/main.php
@@ -73,7 +73,7 @@ $app->group('/api/v2018', 'OpenTHC\POS\Module\API');
 
 
 // Main Page
-$app->group('/dashboard', 'OpenTHC\POS\Module\Dashboard')
+$app->get('', 'OpenTHC\POS\Controller\Dashboard\Main')
 	->add('OpenTHC\POS\Middleware\Auth')
 	->add('OpenTHC\Middleware\Session');
 
