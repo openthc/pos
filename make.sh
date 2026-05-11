c4b13ea6a2dbc72547f7b89ce81495b85dc74a46
openthc <code@openthc.com>
2026-05-11 21:47:55 +0000

    update deps, new make tool

--- make.sh
+++ make.sh
@@ -18,29 +18,12 @@ composer install --no-ansi --no-progress --classmap-authoritative
 
 npm install --ignore-scripts --no-audit --no-fund
 
-# vendor/openthc/common/lib/make.sh bootstrap
-# vendor/openthc/common/lib/make.sh fontawesome
-# vendor/openthc/common/lib/make.sh jquery
-# vendor/openthc/common/lib/make.sh lodash
-# vendor/openthc/common/lib/make.sh htmx
-# vendor/openthc/common/lib/make.sh echarts
-
-php <<PHP
-<?php
-define('APP_ROOT', __DIR__);
-require_once(APP_ROOT . '/vendor/autoload.php');
-\OpenTHC\Make::install_bootstrap();
-\OpenTHC\Make::install_fontawesome();
-\OpenTHC\Make::install_jquery();
-PHP
-
-# lodash
-mkdir -p webroot/vendor/lodash/
-cp node_modules/lodash/lodash.min.js webroot/vendor/lodash/
-
-# htmx
-# mkdir -p webroot/vendor/htmx
-# cp node_modules/htmx.org/dist/htmx.min.js webroot/vendor/htmx/
+vendor/openthc/common/lib/make.sh install_bootstrap
+vendor/openthc/common/lib/make.sh install_fontawesome
+vendor/openthc/common/lib/make.sh install_jquery
+vendor/openthc/common/lib/make.sh install_lodash
+vendor/openthc/common/lib/make.sh install_htmx
+# bash -x /opt/openthc/common/lib/make.sh install_echarts
 
 # qrcode
 mkdir -p webroot/vendor/qrcodejs
@@ -51,7 +34,6 @@ mkdir -p webroot/vendor/chart.js
 cp node_modules/chart.js/dist/chart.umd.js webroot/vendor/chart.js/
 cp node_modules/chart.js/dist/chart.umd.js.map webroot/vendor/chart.js/
 
-#
 # SASS
 ./node_modules/.bin/sass \
 	--fatal-deprecation 1.80.0 \
