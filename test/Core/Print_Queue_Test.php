b14c4f440b36231798eee4b00a09905f0493a283
Matthew Walther <matt@mashio.net>
2026-05-15 15:24:32 -0700

    test: Stub in next step for Print Queue v2

--- test/Core/Print_Queue_Test.php
+++ test/Core/Print_Queue_Test.php
@@ -7,6 +7,30 @@ namespace OpenTHC\POS\Test\Core;
 
 class Print_Queue_Test extends \OpenTHC\POS\Test\Base
 {
+	static function setUpBeforeClass() : void
+	{
+		parent::setUpBeforeClass();
+		// @todo Seed a print queue manually here
+	}
+
+	function zzz_test()
+	{
+		/*
+		@see view/settings/b2c/printer.php
+		$sql = <<<SQL
+		SELECT * FROM auth_company_option WHERE key LIKE 'print-queue%'
+		SQL;
+
+		$res_print_queue = SQL::fetch_all($sql);
+		foreach ($res_print_queue as $row) {
+			// find well-known
+		}
+		*/
+		$dbc = _dbc('user');
+		$queue = $dbc->fetchall('well known print queue');
+	}
+
+	// @see lib/Controller/API/PrintQueue.php
 	function test_read_print_queue()
 	{
 		$rdb = \OpenTHC\Service\Redis::factory();
